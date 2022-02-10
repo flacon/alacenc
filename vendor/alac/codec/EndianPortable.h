9b823ec2381cdbc1ba5d49b6afcc788b895f7754
Alexander Sokolov <sokoloff.a@gmail.com>
2022-02-11 00:59:32 +0300

    Apply clang-format

--- vendor/alac/codec/EndianPortable.h
+++ vendor/alac/codec/EndianPortable.h
@@ -2,19 +2,19 @@
  * Copyright (c) 2011 Apple Inc. All rights reserved.
  *
  * @APPLE_APACHE_LICENSE_HEADER_START@
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  * @APPLE_APACHE_LICENSE_HEADER_END@
  */
 
@@ -48,9 +48,9 @@ float SwapFloat32NtoB(float in);
 double SwapFloat64BtoN(double in);
 double SwapFloat64NtoB(double in);
 
-void Swap16(uint16_t * inUInt16);
-void Swap24(uint8_t * inUInt24);
-void Swap32(uint32_t * inUInt32);
+void Swap16(uint16_t *inUInt16);
+void Swap24(uint8_t *inUInt24);
+void Swap32(uint32_t *inUInt32);
 
 #ifdef __cplusplus
 }
