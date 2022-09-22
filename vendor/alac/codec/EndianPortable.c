143a251899fd6f09b981c11cdcb80b732678a2df
pkubaj <pkubaj@FreeBSD.org>
2022-09-22 18:18:21 +0000

    Generalize LE check
    Fixes build on FreeBSD/powerpc64le.

--- vendor/alac/codec/EndianPortable.c
+++ vendor/alac/codec/EndianPortable.c
@@ -37,6 +37,8 @@
 #define TARGET_RT_LITTLE_ENDIAN 1
 #elif defined(__aarch64__)
 #define TARGET_RT_LITTLE_ENDIAN 1
+#elif __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+#define TARGET_RT_LITTLE_ENDIAN 1
 #elif defined (TARGET_OS_WIN32)
 #define TARGET_RT_LITTLE_ENDIAN 1
 #endif
